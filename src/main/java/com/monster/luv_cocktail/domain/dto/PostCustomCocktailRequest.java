ad0811d0621332621a88e120b1fe815d50221593
skybory <xodus4194@gmail.com>
2024-06-01 11:10:52 +0900

    fix custom cocktail

---
+++ src/main/java/com/monster/luv_cocktail/domain/dto/PostCustomCocktailRequest.java
@@ -0,0 +1,48 @@
+package com.monster.luv_cocktail.domain.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostCustomCocktailRequest {
+
+	private String name;
+	private String description;
+	private MultipartFile image;
+	private String ingredient1;
+	private String ingredient2;
+	private String ingredient3;
+	private String ingredient4;
+	private String ingredient5;
+	private String ingredient6;
+	private String ingredient7;
+	private String ingredient8;
+	private String ingredient9;
+	private String ingredient10;
+	private String ingredient11;
+	private String ingredient12;
+	private String ingredient13;
+	private String ingredient14;
+	private String ingredient15;
+	private String measure1;
+	private String measure2;
+	private String measure3;
+	private String measure4;
+	private String measure5;
+	private String measure6;
+	private String measure7;
+	private String measure8;
+	private String measure9;
+	private String measure10;
+	private String measure11;
+	private String measure12;
+	private String measure13;
+	private String measure14;
+	private String measure15;
+	private Long memberId;
+	private String alcoholic;
+	private String glass;
+}
